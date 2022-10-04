c91dd4f035e93514693d5ba34d1c9ef89757aa40
IB-315168 <315168@via.dk>
2022-10-04 14:39:03 +0200

    Post and Comment models

---
+++ Domain/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int CreatorId { get; }
+        public int PostId { get; }
+        public string Body { get; }
+
+        public Comment(int creatorId, int postId, string body)
+        {
+            CreatorId = creatorId;
+            PostId = postId;
+            Body = body;
+        }
+    }
+}
---
+++ Domain/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int CreatorId { get; }
+        public string Title { get; }
+        public string Body { get; }
+
+        public Post(int creatorId, string title, string body)
+        {
+            CreatorId = creatorId;
+            Title = title;
+            Body = body;
+        }
+    }
+}
